cd49d9fc8db2a0ee406fa117ddcc1a5ee6214c01
Philipp Gaßner <activator112233@gmail.com>
2025-11-04 15:25:31 +0100

    fixed scripts

--- tarascope/scripts/run_macos_textured.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cargo run --bin tarascope custom -o $PWD/output 3 5 0.0 3.0 0.0 0.0 1.0 1.0 1 300 textured $PWD/textures/eye.png
--- tarascope/scripts/run_store_nfs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cargo run --release --bin tarascope random -o /mnt/blender_nfs/tarascope
