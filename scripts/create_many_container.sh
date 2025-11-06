507071d783d6d693297a85f8f27052c146911b7c
Philipp Gaßner <activator112233@gmail.com>
2025-11-06 01:19:11 +0100

    added env

--- scripts/create_many_container.sh
+++ scripts/create_many_container.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+set -a && source .env && set +a
+
 for i in $(seq 1 10);
 do
     docker run --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all -v /mnt/blender_nfs/tarascope:/media -u $(id -u):$(id -g) tarascope tarascope-publisher --output-dir /media
