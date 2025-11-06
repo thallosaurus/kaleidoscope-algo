01e4a4dc9a093e30af7436291e3043216b59d63b
Philipp Gaßner <activator112233@gmail.com>
2025-11-06 01:15:14 +0100

    fixed cmd

--- scripts/create_many_container.sh
+++ scripts/create_many_container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for i in $(seq 1 10);
 do
-    docker run --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all tarascope
+    docker run --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all -v /mnt/blender_nfs/tarascope:/media -u $(id -u):$(id -g) tarascope --output-dir /media
 done
