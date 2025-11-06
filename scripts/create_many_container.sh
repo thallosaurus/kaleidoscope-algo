9f44a69fd1f66bac85dba12a3225b47999307e21
Philipp Gaßner <activator112233@gmail.com>
2025-11-06 01:23:46 +0100

    ahhh

--- scripts/create_many_container.sh
+++ scripts/create_many_container.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-set -a && source .env
+set -a && source .env && set +a
 
 for i in $(seq 1 10);
 do
-    docker run --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all -v /mnt/blender_nfs/tarascope:/media -u $(id -u):$(id -g) tarascope tarascope-publisher --output-dir /media
+    docker run --rm --runtime=nvidia \
+    -e NVIDIA_VISIBLE_DEVICES=nvidia.com/gpu=all \
+    -e PG_HOST=${PG_HOST} \
+    -e PG_USER=${PG_USER} \
+    -e PG_PASS=${PG_PASS} \
+    -e PG_DB=${PG_DB} \
+    -v /mnt/blender_nfs/tarascope:/media \
+    -u $(id -u):$(id -g) \
+    tarascope-publisher --output-dir /media \
 done
