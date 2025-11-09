f04f55050067471855e30bff15ad93793226b2d8
Philipp Gaßner <activator112233@gmail.com>
2025-11-09 16:29:04 +0100

    formatting

--- api-rs/src/main.rs
+++ api-rs/src/main.rs
@@ -1,13 +1,15 @@
 use std::sync::Arc;
 
-use daemon::database::{all_kaleidoscopes, init_database, insert_new_parameterized_job, single_kaleidoscopes};
+use daemon::database::{
+    all_kaleidoscopes, init_database, insert_new_parameterized_job, single_kaleidoscopes,
+};
 use rocket::{State, get, launch, put, routes, serde::json::Json, tokio::sync::Mutex};
 use serde::Deserialize;
 use sqlx::{Pool, Postgres};
 use tarascope::shader::KaleidoArgs;
 
 struct ApiState {
-    pool: Arc<Mutex<Pool<Postgres>>>
+    pool: Arc<Mutex<Pool<Postgres>>>,
 }
 
 #[get("/")]
@@ -44,19 +46,22 @@ async fn random(state: &State<ApiState>) -> String {
 async fn single(state: &State<ApiState>, id: &str) -> String {
     let lock = state.pool.lock().await;
     //let p = lock.acquire().await;
-    let res = single_kaleidoscopes(&lock, &String::from(id)).await.unwrap();
+    let res = single_kaleidoscopes(&lock, &String::from(id))
+        .await
+        .unwrap();
 
     serde_json::to_string(&res).unwrap()
 }
 
-
 #[launch]
 async fn rocket() -> _ {
     let _ = dotenv::dotenv().ok();
-    
+
     let pool = init_database().await.unwrap();
 
-    rocket::build().manage(ApiState {
-        pool: Arc::new(Mutex::new(pool))
-    }).mount("/api", routes![full, single, new, random])
+    rocket::build()
+        .manage(ApiState {
+            pool: Arc::new(Mutex::new(pool)),
+        })
+        .mount("/api", routes![full, single, new, random])
 }
